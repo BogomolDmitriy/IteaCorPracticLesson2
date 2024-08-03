0cb72415d0bd015854cfa67d5d209470f3f615b3
Dmitriy Bohomol <bogomoldima1992@gmail.com>
2024-08-03 10:57:10 +0300

    Implemented Books controller metods+

---
+++ IteaCorPracticLesson2/Book.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IteaCorPracticLesson2
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Pages { get; set; }
+    }
+}
